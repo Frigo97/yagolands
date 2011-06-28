0c3cb3ef12a79696042140166157fc4e99d17367
Simone (Demo) Gentili <sensorario@gmail.com>
2011-06-28 15:09:17 +0200

    some stuffs

--- models/MRuoli.php
+++ models/MRuoli.php
@@ -1,9 +1,18 @@
 <?php
 
+
+/**
+ * This is role model
+ * 
+ */
 class MRuoli extends Model {
 
-    public function __construct() {
-        parent::__construct('ruoli');
-    }
+  /**
+   * Tell to Model super class the name of the table of this model.
+   */
+  public function __construct () {
+    parent::__construct ( 'ruoli' );
+
+  }
 
 }
