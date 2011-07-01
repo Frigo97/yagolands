6a6490531f64aacfc98ceb0b33389b32134543b3
Simone (Demo) Gentili <sensorario@gmail.com>
2011-07-01 07:49:35 +0200

    Resources cannot grow beyond the limit of the capacity of the warehouse'

--- classes/Yago2.php
+++ classes/Yago2.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * Questa è la classe di bootstrap. In questo luogo viene caricato il database,
  * vengono gestite le azioni di post, caricato il backgrouind e così via.
@@ -10,9 +9,8 @@
  */
 class Yago2 extends Controller {
 
-  public static function getVersion () {
+  public static function getVersion() {
     return "2.0";
-
   }
 
   public $layout = 'main';
@@ -20,30 +18,30 @@ class Yago2 extends Controller {
   public $action = 'vista';
   public $contest = 'yago';
 
-  public function __construct () {
+  public function __construct() {
 
-    @session_start ();
+    @session_start();
 
     /**
      * conto il numero di parametri passati
      */
     $count = 0;
-    foreach ( $_GET as $this->contest => $key ) {
-      $count ++;
+    foreach ($_GET as $this->contest => $key) {
+      $count++;
     };
 
     /**
      * Se è solo uno carico da quello il controller
      * e l'action
      */
-    if ( $count == 1 ) {
+    if ($count == 1) {
       list(
               $this->controller,
-              $this->action) = explode ( "/", $key );
+              $this->action) = explode("/", $key);
     }
 
     $classname = $this->controller;
-    if ( ! class_exists ( $classname ) ) {
+    if (!class_exists($classname)) {
       $this->controller = 'error';
       $this->action = 'nocontroller';
     }
@@ -53,57 +51,52 @@ class Yago2 extends Controller {
      * Il controller caricherà i model giusti.
      * 
      */
-//    Log::save ( array (
-//        'string' => $this->controller . '::' . $this->action . '();'
-//    ) );
-    $controller = ucfirst ( $this->controller );
-    $action = 'action' . (ucfirst ( $this->action ));
+    $controller = ucfirst($this->controller);
+    $action = 'action' . (ucfirst($this->action));
     $obj = new $controller();
 
     /**
      * Verifico che l'action esista davvero.
      */
-    if ( method_exists ( $obj, $action ) ) {
-      $obj->$action ( &$this );
+    if (method_exists($obj, $action)) {
+      $obj->$action(&$this);
     } else {
       $this->controller = 'error';
       $this->action = 'noaction';
     }
-
   }
 
-  protected function render () {
+  protected function render() {
 
 
     $filename = 'views/contents/' . ($this->controller) . '/' . ($this->action) . '.php';
-    if ( file_exists ( $filename ) ) {
-      ob_start ();
+    if (file_exists($filename)) {
+      ob_start();
       include $filename;
-      $content = ob_get_clean ();
+      $content = ob_get_clean();
     } else {
-      Log::save ( array (
+      Log::save(array(
           'string' => 'Si sta cercando di caricare una view non disponibile: ' . ($filename),
           'livello' => 'errore'
-      ) );
+      ));
     }
 
     $filename = 'views/layouts/' . ($this->layout) . '.php';
-    if ( file_exists ( $filename ) ) {
-      ob_start ();
+    if (file_exists($filename)) {
+      ob_start();
       include $filename;
-      $layout = ob_get_clean ();
+      $layout = ob_get_clean();
     } else {
-      Log::save ( array (
+      Log::save(array(
           'string' => 'Si sta cercando di caricare un layout non disponibile: ' . ($filename),
           'livello' => 'errore'
-      ) );
+      ));
     }
 
-    die ( $layout );
-
+    die($layout);
   }
 
-  public static function run () {
+  public static function run() {
 
     /**
      * This code fix this Warning: Warning: date() [function.date]: It is 
@@ -115,7 +108,7 @@ class Yago2 extends Controller {
      * 'CEST/2.0/DST' instead in 
      * /Library/WebServer/Documents/yago2/classes/Log.php on line 21
      */
-    $date_default_timezone_set = date_default_timezone_set ( 'Europe/Rome' );
+    $date_default_timezone_set = date_default_timezone_set('Europe/Rome');
 
     $pdo = new Model;
     $utenti = new MUtenti;
@@ -126,63 +119,67 @@ class Yago2 extends Controller {
     /**
      * Questo cronjob ha il compito di controllare le code di costruzione.
      */
-    foreach ( $pdo->query ( 'select * from codadiaddestramento where fineaddestramento <= \'' . (date ( 'Y-m-d H:i:s' )) . '\'' ) as $addestramentoFinito ) {
-
-      $esercito->addOne ( $addestramentoFinito['idtruppa'] );
-
-      $pdo->query ( 'delete from codadiaddestramento where id = ' . $addestramentoFinito['id'] );
+    foreach ($pdo->query('select * from codadiaddestramento where fineaddestramento <= \'' . (date('Y-m-d H:i:s')) . '\'') as $addestramentoFinito) {
+      $esercito->addOne($addestramentoFinito['idtruppa'], $addestramentoFinito['idutente']);
+      $pdo->query('delete from codadiaddestramento where id = ' . $addestramentoFinito['id']);
     };
 
     /**
      * Questo cronjob ha il compito di controllare le code di costruzione.
      */
-    foreach ( $pdo->query ( 'select * from codadicostruzione where finelavori <= \'' . (date ( 'Y-m-d H:i:s' )) . '\'' ) as $lavorofinito ) {
+    foreach ($pdo->query('select * from codadicostruzione where finelavori <= \'' . (date('Y-m-d H:i:s')) . '\'') as $lavorofinito) {
       $costruzione = new MCostruzioni;
-      $costruzione->update ( array (
-          'datafinelavoro' => date ( 'Y-m-d H:i:s' ),
-          'mktimefinelavoro' => mktime (),
+      $costruzione->update(array(
+          'datafinelavoro' => date('Y-m-d H:i:s'),
+          'mktimefinelavoro' => mktime(),
           'idutente' => $lavorofinito['idutente'],
           'idedificio' => $lavorofinito['idedificioincostruzione'],
           'livello' => $lavorofinito['livelloincostruzione'],
           'x' => $lavorofinito['x'],
           'y' => $lavorofinito['y'],
-              ), array (
+              ), array(
           'x' => $lavorofinito['x'],
           'y' => $lavorofinito['y'],
-      ) );
-      $pdo->query ( 'delete from codadicostruzione where id = ' . $lavorofinito['id'] );
+      ));
+      $pdo->query('delete from codadicostruzione where id = ' . $lavorofinito['id']);
     };
 
     /**
      * Questo cronjob ha il compito di accrescere le risorse di un utente.
      */
-    foreach ( $edifici->findAll ( array ( 'id', 'camporisorsa' ), array ( 'risorsa' => 1 ) ) as $itemEdificio ) {
-      foreach ( $costruzioni->find ( array ( 'id', 'idutente', 'mktimefinelavoro', 'livello' ), array ( 'idedificio' => $itemEdificio['id'] ) ) as $itemCostruzione ) {
-        $secondipassati = mktime () - $itemCostruzione['mktimefinelavoro'];
-        $secondiperrisorsa = (int) (3600 / Config::risorseAllOra ( $itemCostruzione['livello'] ));
+    foreach ($edifici->findAll(array('id', 'camporisorsa'), array('risorsa' => 1)) as $itemEdificio) {
+      foreach ($costruzioni->find(array('id', 'idutente', 'mktimefinelavoro', 'livello'), array('idedificio' => $itemEdificio['id'])) as $itemCostruzione) {
+        $secondipassati = mktime() - $itemCostruzione['mktimefinelavoro'];
+        $secondiperrisorsa = (int) (3600 / Config::risorseAllOra($itemCostruzione['livello']));
         $nomeRisorsa = $itemEdificio['camporisorsa'];
         $unitadaaggiungere = $secondipassati / $secondiperrisorsa;
         $temporimanente = ($unitadaaggiungere) - ((int) ($unitadaaggiungere));
-        $minutiperrisorsa = (int) (60 / Config::risorseAllOra ( $itemCostruzione['livello'] ));
-        if ( $secondipassati > $secondiperrisorsa ) {
+        $minutiperrisorsa = (int) (60 / Config::risorseAllOra($itemCostruzione['livello']));
+        if ($secondipassati > $secondiperrisorsa) {
           try {
             $resto = @$secondipassati % @$secondiperrisorsa;
-          } catch ( Exception $E ) {
+          } catch (Exception $E) {
             $resto = 0;
           }
           $unità = ($secondipassati - $resto) / $secondiperrisorsa;
           // Log::save ( array ( 'string' => 'Devo aggiungere ' . ($unità) . ' unità di ' . $nomeRisorsa . ' e rimarranno fuori ' . ($resto) . ' secondi.' ) );
-          $risorseUtente = Config::getRisorseUtente ( $itemCostruzione['idutente'] );
+          $costruzioni = new MCostruzioni;
+          $edifici = new MEdifici;
+          $idEdificio = $edifici->getId('magazzino');
+          $livello = $costruzioni->getLivello($idEdificio);
+          $capienzaMassima = Config::moltiplicatoreCapienzaEdificio($livello);
+          $risorseUtente = Config::getRisorseUtente($itemCostruzione['idutente']);
           $risorseUtente[$nomeRisorsa] += $unità;
-          $utenti->update ( $risorseUtente, array ( 'id' => $itemCostruzione['idutente'] ) );
-          $costruzioni->update ( array ( 'mktimefinelavoro' => mktime () + $resto ), array ( 'id' => $itemCostruzione['id'] ) );
+          if ($risorseUtente[$nomeRisorsa] > $capienzaMassima)
+            $risorseUtente[$nomeRisorsa] = $capienzaMassima;
+          $utenti->update($risorseUtente, array('id' => $itemCostruzione['idutente']));
+          $costruzioni->update(array('mktimefinelavoro' => mktime() + $resto), array('id' => $itemCostruzione['id']));
         }
       }
     }
 
     $obj = new Yago2();
-    $obj->render ();
-
+    $obj->render();
   }
 
   /**
@@ -196,10 +193,9 @@ class Yago2 extends Controller {
    * @param string $permission 
    * @return boolean
    */
-  public function can ( $permission = '' ) {
+  public function can($permission = '') {
 
-    return UtenteWeb::status ()->user->$permission || 0;
-
+    return UtenteWeb::status()->user->$permission || 0;
   }
 
 }
