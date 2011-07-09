ce8c1a97994ecad3f42ee15da982cbf7d8fb735f
Simone (Demo) Gentili <sensorario@gmail.com>
2011-07-09 14:06:46 +0200

    Added instructions in home page.

--- views/contents/site/login.php
+++ views/contents/site/login.php
@@ -10,8 +10,12 @@
 
   <div id="contenitore">
     <div style="margin-left: 200px; margin-top: 10px; position: absolute;">
+      <h1>Come si gioca?</h1>
+      <p>Quando si entra dentro il mondo di yago, verrà generato un esagono per il tuo utente. Verrà generato un esagono anche per ciascuno dei 6 lati attorno a te.</p>
+      <p>Per prima cosa dovrai costruire i campi delle risorse (ferro, grano, legno, roccia). Solo allora sarà possibile sbloccare il centro del villaggio. Ogni volta che costruirai qualche cosa, i tuoi possedimenti cresceranno sempre di più.</p>
+      <p>Una volta costruito anche quello, potrai procedere con caserma, se vorrai attaccare i villaggi nemici, oppure mercato, per effettuare scambi commerciali.</p>
       <h1>Attenzione!</h1>
-      yagolands è ancora in beta test. Per provarlo puoi trovare le istruzioni <a href="https://github.com/sensorario/yagolands/wiki/Try-yago" target="_blank">qui</a>. Se preferisci puoi provare l'accounte di test <strong>semola</strong> ed inseire come password <strong>password</strong>.
+      <p>yagolands è ancora in beta test. Per provarlo puoi trovare le istruzioni <a href="https://github.com/sensorario/yagolands/wiki/Try-yago" target="_blank">qui</a>. Se preferisci puoi provare l'accounte di test <strong>semola</strong> ed inseire come password <strong>password</strong>.</p>
     </div>
     <div id="side-left">
       <h1>Login</h1>
