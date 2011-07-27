8a65e598b4f235777eb5e87fa3c27939e064c3d6
Simone (Demo) Gentili <sensorario@gmail.com>
2011-07-28 00:17:54 +0200

    aggiornamento degli edifici in costruzione quando termina un timer

--- javascript/yago.js
+++ javascript/yago.js
@@ -30,7 +30,11 @@ $(document).ready(function(){
           for(i=0;i<data.costruzioniincoda;i++) {
             $('#the_final_countdown_'+i+'_text').createTimer({
               time_in_seconds: $('#the_final_countdown_'+i).val(),
-              time_format: 'HH:MM:ss'
+              time_format: 'HH:MM:ss',
+              buzzer: function () {
+                Yago.callEndCodaCalled = false;
+                this.callEndCoda();
+              }
             });
           }
           Yago.callEndCodaCalled = true;
