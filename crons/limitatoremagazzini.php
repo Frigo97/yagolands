910762376d13eb9d7eae75a9f48ac746bb7cdafd
Simone <sensorario@gmail.com>
2011-08-06 16:25:05 +0200

    indentato al meglio il limitatore dei magazzini

--- crons/limitatoremagazzini.php
+++ crons/limitatoremagazzini.php
@@ -16,22 +16,14 @@ $edifici = new MEdifici;
 $esercito = new MEsercito;
 
 foreach ($utenti->findAll() as $itemUtenti) {
-
   foreach (array('magazzino', 'granaio') as $itemContenitore) {
-
     $idCostruzione = $costruzioni->getIdCostruzione($edifici->getId($itemContenitore), $itemUtenti['id']);
-
     $livello = $costruzioni->getLivelloProprietario($idCostruzione, $itemUtenti['id']);
-
     $capienzaMassima = Config::moltiplicatoreCapienzaEdificio($livello);
-
     $risorseUtente = Config::getRisorseUtente($itemUtenti['id']);
-
     $arrayRisorse = $itemContenitore == 'magazzino' ? array('ferro', 'legno', 'roccia') : array('grano');
-
     foreach ($arrayRisorse as $itemRisorse) {
       $nomeRisorsa = $itemRisorse;
-
       if ($risorseUtente[$nomeRisorsa] > $capienzaMassima) {
         $risorseUtente[$nomeRisorsa] = $capienzaMassima;
       }
